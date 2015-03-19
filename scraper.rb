66602da6a31801273fba8cc950ed05b43ea1526e
Matthew Landauer <matthew@oaf.org.au>
2015-03-19 12:17:33 +1100

    bundle update and switch to given url

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/@@horizondap_lpsc@@/atdis/1.0/applications.json')
+ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/lpsc/atdis/1.0')
