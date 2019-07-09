824481bbce48dd180c9271c096f515ecb74cfe57
Matthew Landauer <mlandauer@gmail.com>
2019-07-09 10:39:21 +1000

    Switch over to OAF version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,3 @@
-require 'scraperwiki'
 require 'horizon_xml'
 
-collector = Horizon_xml.new
-collector.base_url    = 'http://myhorizon.solorient.com.au/Horizon/'
-collector.domain      = 'horizondap_lpsc'
-collector.comment_url = 'mailto:lpsc@lpsc.nsw.gov.au'
-collector.period      = ENV['MORPH_PERIOD']
-
-collector.getRecords.each do |record|
-#   p record
-  puts "Saving record " + record['council_reference'] + ", " + record['address']
-  ScraperWiki.save_sqlite(['council_reference'], record)
-end
+Horizon_xml.scrape_and_save(:liverpool_plains)
